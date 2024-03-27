d03fb86318642b777ef55dcb9d65e0f2a138180c
blink1197 <cgbagotsay11@gmail.com>
2024-03-27 19:55:05 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,178 @@
+const downloadButton = document.getElementById('downloadImage');
+const submitButton = document.getElementById('submit');
+
+let fileName = '';
+let numberOfPixels;
+let messageBinaryString;
+let messageBinaryStringArray;
+let secretCode;
+let message;
+
+function handleImageUpload() {
+    const fileInput = document.getElementById('imageUpload');
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert('Please select an image file.');
+        return;
+    }
+
+    fileName = file.name;
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const imageData = event.target.result;
+        localStorage.setItem(fileName, imageData);
+    };
+    reader.readAsDataURL(file);
+}
+
+
+function downloadImage() {
+    const imageData = localStorage.getItem(fileName);
+    if (imageData) {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = imageData;
+        downloadLink.download = 'image-embedded.png';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    } else {
+        alert('No image available in local storage.');
+    }
+}
+
+
+function stringToBinary(str) {
+    let binary = '';
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i).toString(2);
+        binary += '0'.repeat(8 - charCode.length) + charCode;
+    }
+    return binary;
+}
+
+
+function binaryToString(binary) {
+    let str = '';
+    for (let i = 0; i < binary.length; i += 8) {
+        let byte = binary.substr(i, 8);
+        str += String.fromCharCode(parseInt(byte, 2));
+    }
+    return str;
+}
+
+
+function getMessageAndCode() {
+    message = document.getElementById("message").value;
+    messageBinaryString =  stringToBinary(message);
+    messageBinaryStringArray = messageBinaryString.split("");
+    secretCode = document.getElementById("secret").value;
+}
+
+
+function manipulateImage() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var fileInput = document.getElementById('imageUpload');
+    var img = new Image();
+
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var data = imageData.data;
+        var data_copy = data;
+        
+
+        numberOfPixels = data.length
+
+        console.log(messageBinaryStringArray);
+        const pixels = selectNumbers(numberOfPixels, messageBinaryString.length, secretCode)
+        console.log(pixels);
+
+
+        for (let pixel of pixels) {
+            console.log(data[pixel]);
+        }
+
+
+        let i = 0;
+        
+        for (let pixel of pixels) {
+
+            if (messageBinaryStringArray[i] == '1') {
+                data_copy[pixel] += 1;
+            }
+
+            i ++;
+            console.log(data_copy[pixel]);
+        }
+        //console.log(data_copy)
+      };
+      img.src = event.target.result;
+    };
+
+    // Read uploaded image as data URL
+    reader.readAsDataURL(fileInput.files[0]);
+}
+
+
+function selectNumbers(N, M, seed) {
+    // Generate a numeric seed from the string using a hash function
+    const numericSeed = stringToSeed(seed);
+
+    // Initialize the pseudo-random number generator with the numeric seed
+    const rng = new Math.seedrandom(numericSeed);
+
+    let numbers = [];
+    for (let i = 0; i <= N; i++) {
+        numbers.push(i);  // Create an array of numbers from 0 to N
+    }
+    // Shuffle the array using Fisher-Yates algorithm with the initialized PRNG
+    for (let i = numbers.length - 1; i > 0; i--) {
+        const j = Math.floor(rng() * (i + 1));
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+    }
+    // Select the first M numbers from the shuffled array
+    let selectedNumbers = numbers.slice(0, M);
+    return selectedNumbers;
+}
+
+// Function to convert a string to a numeric seed
+function stringToSeed(str) {
+    let seed = 0;
+    for (let i = 0; i < str.length; i++) {
+        seed += str.charCodeAt(i);
+    }
+    return seed;
+}
+
+
+
+function divideStringIntoGroups(str) {
+    let nibbles = [];
+    for (let i = 0; i < str.length; i += 4) {
+        nibbles.push(str.substring(i, i + 4));
+    }
+    return nibbles;
+}
+
+
+
+
+  
+
+
+
+downloadButton.addEventListener("click", downloadImage);
+submitButton.addEventListener("click", function() {
+    handleImageUpload();
+    getMessageAndCode();
+    manipulateImage();
+});
